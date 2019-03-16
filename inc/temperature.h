84ab1bf65fa7b2e6353b9e0971fddac542292bd2
Souvik De <souvik.de@colorado.edu>
2019-03-16 15:43:07 -0600

    Modified header file temperature.h

--- inc/temperature.h
+++ inc/temperature.h
@@ -1,7 +1,20 @@
-#include<stdio.h>
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  		temperature.h
+​ * ​ ​ @brief​ ​ 		Contains dependencies pertaining to temperature.c and
+ *					the temperature sensor TMP102.
+ *	 @Comm Type 	I2C
+ *   @Tools_Used 	ARM-LINUX-GCC
+​ * ​ ​ @Author(s)​  	​​Souvik De, Devansh Mittal
+​ * ​ ​ @Date​ ​​ 		March 16th, 2019
+​ * ​ ​ @version​ ​ 		1.0
+*****************************************************************************/
 
-int main()
-{
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
 
-	return 0;
-}
+#define TMP102_DEV_ID	(0x48)
+
+int init_temperature(void);
+float read_temperature(int fd, int type);
+
+#endif
