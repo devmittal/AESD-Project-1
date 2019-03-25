c6143c586d5ed28b4d840f137b4e8c570696e935
Souvik De <souvik.de@colorado.edu>
2019-03-24 18:34:08 -0600

    Removed bugs.

--- inc/i2c.h
+++ inc/i2c.h
@@ -30,6 +30,6 @@ int init_i2c(uint8_t DeviceID);
 uint8_t write_i2c(int fd, uint8_t register_type);
 uint8_t* read_i2c16(int fd);
 uint8_t read_i2c8(int fd);
-int close_i2c(int fd);
+void close_i2c(int fd);
 
 #endif
