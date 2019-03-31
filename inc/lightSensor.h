b6f9ad31accd8c47a03bae97e5a9c562e8f9913a
Devansh Mittal <demi1671@colorado.edu>
2019-03-31 16:10:50 -0600

    added comments

--- inc/lightSensor.h
+++ inc/lightSensor.h
@@ -31,6 +31,7 @@
 #define INTERRUPT_DISABLE (0x00)
 #define THRESHOLD (1.0)
 
+/* Structure containing data relevant to light sensor */
 typedef struct Light
 {
 	int lux_visiblelight;
@@ -41,18 +42,112 @@ typedef struct Light
 	int IsError;
 }light_t;
 
+/**
+* @Brief  This function checks the identification register as part of a startup test and confirms
+* 		  if the light sensor is working properly.
+* @Param  void
+* @Return ID register contents/Error
+**/
 int startup_test(void);
+
+/**
+* @Brief  This function powers up the light sensor.
+* @Param  void
+* @Return Control register contents/Error
+**/
 int power_up(void);
+
+/**
+* @Brief  This function reads control register of the light sensor.
+* @Param  void
+* @Return Control register contents
+**/
 uint8_t read_control_register(void);
+
+/**
+* @Brief  This function is used to set the intergation time and gain of light sensor.
+* @Param  integ: Integration time(0-3); gain: gain(0-1)
+* @Return New integration time and gain written into timing control register of light sensor
+**/
 uint8_t set_timing_register(uint8_t integ, uint8_t gain);
+
+/**
+* @Brief  This function reads timing register of the light sensor.
+* @Param  void
+* @Return timing register contents
+**/
 uint8_t read_timing_register(void);
+
+/**
+* @Brief  This function enables interrupts in the interrupt control register of the light sensor.
+* @Param  interrupt_persist: value to control when interrupts occur (0-15)
+* @Return void
+**/
 void enable_interrupt(uint8_t interrupt_persist);
+
+/**
+* @Brief  This function disable interrupts of the light sensor.
+* @Param  void
+* @Return void
+**/
 void disable_interrupt(void);
+
+/**
+* @Brief  This function sets the high and low interrupt threshold of the light sensor.
+* @Param  thres_low: 16 bit low threshold value; thres_high: 16 bit high threshold value
+* @Return void
+**/
 void set_interrupt_threshold(uint16_t thresh_low, uint16_t thresh_high);
+
+/**
+* @Brief  This function reads the high and low interrupt threshold of the light sensor.
+* @Param  void
+* @Return address of array. 1st element consists of low threshold value. 2nd element of array
+* consists of high threshold value
+**/
 uint16_t* read_interrupt_threshold(void);
+
+/**
+* @Brief  This function reads channel 0 of light sensor.
+* @Param  void
+* @Return 16 bit channel 0 lux value or error status
+**/
 int read_visible_light(void);
+
+/**
+* @Brief  This function reads channel 1 of light sensor.
+* @Param  void
+* @Return 16 bit channel 1 lux value or error status
+**/
 int read_IR_light(void);
+
+/**
+* @Brief  This function calculates actual luminosity based on channel 0 and channel 1 lux values
+*		  of light sensor.
+* @Param  ch0: channel 0 lux value; ch1: channel 1 lux value
+* @Return actual luminosity
+**/
 double cal_lumen(int ch0, int ch1);
+
+/**
+* @Brief  This function checks if there is any change in light state from the previous state.
+* @Param  isLight: Current state (light or dark)
+* @Return 1 - change from previous state; 0 - no change from previous state
+**/
 uint8_t change(uint8_t isLight);
+
+/**
+* @Brief  This function determines current light state based on threshold value.
+* @Param  lumen: Calculated actual luminosity
+* @Return 1 - light; 0 - dark
+**/
 uint8_t state(double lumen);
+
+/**
+* @Brief  This function calls functions to read channel 0 and 1, calculate lumen, determine
+*		  current light state, and if there was a change from previous state and stores it
+*		  in the allocated structure.
+* @Param  void
+* @Return the structure object
+**/
 light_t read_LightSensor(void);
--- inc/temperature.h
+++ inc/temperature.h
@@ -16,6 +16,7 @@
 
 #define SCALING_FACTOR	(0.0625)
 
+/* Structure containing data relevant to temperature sensor */
 typedef struct Temperature
 {
 	float celcius;
@@ -24,19 +25,104 @@ typedef struct Temperature
 	int IsError;
 }tempt_t;
 
+/* Pointer Addresses */
 enum Registor_Address{TEMPERATURE_REG, CONFIGURATION_REG, TLOW_REG, THIGH_REG};
 
+/**
+* @Brief  This function reads the temperature register of the temperature sensor
+* @Param  void
+* @Return the temperature structure object
+**/
 tempt_t read_temperature(void);
+
+/**
+* @Brief  This function calculates actual temperature in celsius, fahrenheit, and kelvin
+*		  based on digital temperature values read from the temperature register
+* @Param  temperature_hex: digital temperature values read from the temperature register
+		  celcius: store calculated temperature in celsius
+		  farenheit: store calculated temperature in fahrenheit
+		  kelvin: store calculated temperature in kelvin
+* @Return void
+**/
 void cal_temp(int16_t temperature_hex, float *celcius, float *farenheit, float *kelvin);
+
+/**
+* @Brief  This function reads the lower temperature limit from the tlow register
+* 		  of the temperature sensor
+* @Param  void
+* @Return lower temperature limit
+**/
 float read_Tlow(void);
+
+/**
+* @Brief  This function reads the higher temperature limit from the thigh register
+* 		  of the temperature sensor
+* @Param  void
+* @Return higher temperature limit
+**/
 float read_Thigh(void);
+
+/**
+* @Brief  This function reads the configuration register of the temperature sensor
+* @Param  void
+* @Return configuration register contents or error status
+**/
 int read_configuration_reg(void);
+
+/**
+* @Brief  This function sets the shutdown mode in the configuration register of the
+* 		  temperature register
+* @Param  void
+* @Return void
+**/
 void set_shutdown(void);
+
+/**
+* @Brief  This function disables the shutdown mode in the configuration register of the
+* 		  temperature register
+* @Param  void
+* @Return void
+**/
 void disable_shutdown(void);
+
+/**
+* @Brief  This function reads the fault bits from the configuration register of the
+* 		  temperature register
+* @Param  void
+* @Return value of fault bits
+**/
 uint8_t read_fault(void);
+
+/**
+* @Brief  This function reads the extended mode bit in the configuration register of the
+* 		  temperature register
+* @Param  void
+* @Return Extended Mode bit value
+**/
 uint8_t read_em(void);
+
+/**
+* @Brief  This function sets/disables the extended mode in the configuration register of the
+* 		  temperature register
+* @Param  em: Normal mode operation(0)/ Extended mode operation(1)
+* @Return void
+**/
 void write_em(uint8_t em);
+
+/**
+* @Brief  This function reads the conversion rate bits in the configuration register of the
+* 		  temperature register
+* @Param  void
+* @Return value of conversion rate bits
+**/
 uint8_t read_conversion_rate(void);
+
+/**
+* @Brief  This function sets the conversion rate bits in the configuration register of the
+* 		  temperature register
+* @Param  cr: conversion rates (0-3)
+* @Return void
+**/
 void set_conversion_rate(uint8_t cr);
 
 #endif
