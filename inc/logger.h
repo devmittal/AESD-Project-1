71b0cbbfae2fd6b1f2a00577254319859cbb86bc
Souvik De <43248539+svikde@users.noreply.github.com>
2019-03-31 19:50:33 -0600

    Update logger.h

--- inc/logger.h
+++ inc/logger.h
@@ -1,12 +1,12 @@
 /*****************************************************************************
-​ ​* ​ ​@file​ ​  		logger.c
-​ * ​ ​@brief​ ​ 		Header file for Logger thread containing defines pertain-
- *					to logger.c.
- *  @Tools_Used 	ARM-LINUX-GCC
-​ * ​ ​@Author(s)​  	​​Souvik De, Devansh Mittal
-​ * ​ ​@Date​ ​​ 			March 16th, 2019
- *	@Modified		March 24th, 2019
-​ * ​ ​@version​ ​ 		1.1
+ *  @file          logger.c
+ *  @brief         Header file for Logger thread containing defines pertain-
+ *					      to logger.c.
+ *  @Tools_Used   ARM-LINUX-GCC
+ *  @Author(s)    Souvik De, Devansh Mittal
+ *  @Date         March 16th, 2019
+ *	@Modified     March 24th, 2019
+ *  @version      1.1
 *****************************************************************************/
 
 #ifndef LOGGER_H
